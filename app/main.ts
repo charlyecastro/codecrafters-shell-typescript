e5ff0742ae937300f530d8508410a41fab3166e2
charlye <charlyecastro@gmail.com>
2026-03-09 01:10:11 -0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,17 @@
 import { createInterface } from "readline";
+import { access, constants } from 'fs/promises';
+
+const commands = ["exit","type","echo"]
+
+async function checkExecutePermission(path: string): Promise<boolean> {
+  try {
+    // Check if the path is executable (X_OK)
+    await access(path, constants.X_OK);
+    return true;
+  } catch {
+    return false;
+  }
+}
 
 const rl = createInterface({
   input: process.stdin,
@@ -14,12 +27,30 @@ rl.on('line', (command: string) => {
     rl.close();
     return;
   }
+
   const [mainCommand, ...args] = command.split(' ');
+
+  if (!commands.includes(mainCommand)) {
+    console.log(`${mainCommand}: command not found`);
+  }
+
   // handle Type
   if (mainCommand === "type") {
     const secondCommand = args[0];
-    if (secondCommand === "echo" || secondCommand === "exit" || secondCommand === "type") {
+    const systemPath: string = process.env.Path;
+    const systemPaths = systemPath.split(":");
+    const foundPaths = systemPaths.filter( async (path: string) => {
+      const found = path.includes(secondCommand);
+      if (!found) return false
+      return await checkExecutePermission(path)
+    } )
+    
+    const foundPath = foundPaths[0]
+
+    if (commands.includes(mainCommand)) {
       console.log(`${args[0]} is a shell builtin`)
+    } else if(foundPath){
+      console.log(`${secondCommand} is ${foundPath}`)
     } else {
       console.log(`${secondCommand}: not found`);
     }
@@ -27,8 +58,7 @@ rl.on('line', (command: string) => {
   // handle Echo
   else if (mainCommand === "echo") {
     console.log(args.join(" ")); 
-  } else {
-    console.log(`${mainCommand}: command not found`);
-  }
+  } 
+
   rl.prompt();
 });
