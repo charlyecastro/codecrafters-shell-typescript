af07118fb5aab9421df96a8d5504bb9679caf346
charlye <charlyecastro@gmail.com>
2026-03-09 01:51:36 -0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -38,9 +38,9 @@ async function parseCommand(command: string){
       return
     } 
 
-    const userPath = process.env.Path
+    const userPath = process.env.PATH
     if (!userPath) {
-      console.log(`${secondCommand}: not found userPath fail`)
+      console.log(`${secondCommand}: not found`)
       return;
     }
 
@@ -51,11 +51,11 @@ async function parseCommand(command: string){
         await access(filePath, constants.X_OK)
         console.log(`${secondCommand} is ${filePath}`)
       } catch {
-        console.log(`${secondCommand}: not found access fail`);
+        console.log(`${secondCommand}: not found`);
       }
     }
 
-    console.log(`${secondCommand}: not found fall back`);
+    console.log(`${secondCommand}: not found`);
   }
   // handle Echo
   if (mainCommand === "echo") {
