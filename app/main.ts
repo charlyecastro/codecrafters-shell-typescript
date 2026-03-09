e11efe94742ae6cc7f7194607e10565f8236740c
charlye <charlyecastro@gmail.com>
2026-03-09 00:29:01 -0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -8,13 +8,22 @@ const rl = createInterface({
 
 
 rl.prompt();
+
 rl.on('line', (command: string) => {
+  // handle Exit
   if (command === 'exit') {
     rl.close();
     return;
   }
   const [cmd, ...args] = command.split(' ');
-  if (cmd === "echo") {
+  // handle Type
+  if (cmd === "type") {
+    if (args[0] === "echo" || args[0] === "exit") {
+      console.log(`${args[0]} is a shell builtin`)
+    }
+  }
+  // handle Echo
+  else if (cmd === "echo") {
     console.log(args.join(" ")); 
   } else {
     console.log(`${command}: command not found`);
